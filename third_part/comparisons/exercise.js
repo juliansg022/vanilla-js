6011f827517907ee7cab8e5f11bca926aa69b070
Julián David Sánchez Gallego <julian@kommit.co>
2021-12-30 11:21:45 -0500

    Add the comparisons exercise

---
+++ third_part/comparisons/exercise.js
@@ -0,0 +1,9 @@
+// What will be the result for these expressions?
+
+alert(5 > 4); // Displays "true"
+alert("apple" > "pineapple"); // Displays "false"
+alert("2" > "12"); // Displays "true"
+alert(undefined == null); // Displays "true"
+alert(undefined === null); // Displays "false"
+alert(null == "\n0\n"); // Displays "false"
+alert(null === +"\n0\n"); // Displays "false"
