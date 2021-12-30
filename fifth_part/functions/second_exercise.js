293a63254c05f24927066fdffece19de102c6799
Julián David Sánchez Gallego <julian@kommit.co>
2021-12-30 16:49:38 -0500

    Add exercises building functions

---
+++ fifth_part/functions/second_exercise.js
@@ -0,0 +1,34 @@
+// The following function returns true if the parameter age is greater than 18.
+
+// Otherwise it asks for a confirmation and returns its result.
+
+// function checkAge(age) {
+//   if (age > 18) {
+//     return true;
+//   } else {
+//     return confirm("Did parents allow you?");
+//   }
+// }
+
+// Rewrite it, to perform the same, but without if, in a single line.
+
+// Make two variants of checkAge:
+
+// Using a question mark operator ?
+// Using OR ||
+
+function checkAgeV1(age) {
+  return age > 18 ? true : confirm("Did you parents allow you?");
+}
+
+function checkAgeV2(age) {
+  return age > 18 || confirm("Did you parents allow you?");
+}
+
+let currentAge = prompt("What's your age?");
+
+let answerV1 = checkAgeV1(currentAge);
+let answerV2 = checkAgeV2(currentAge);
+
+alert(`The value saved in the V1 is ${answerV1}`);
+alert(`The value saved in the V2 is ${answerV2}`);
---
+++ fifth_part/functions/third_exercise.js
@@ -0,0 +1,15 @@
+// Write a function min(a,b) which returns the least of two numbers a and b.
+
+// For instance:
+
+// min(2, 5) == 2
+// min(3, -1) == -1
+// min(1, 1) == 1
+
+function min(a, b) {
+  return a < b ? a : b;
+}
+
+alert(min(2, 5) == 2);
+alert(min(3, -1) == -1);
+alert(min(1, 1) == 1);
