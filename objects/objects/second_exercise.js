65cdca892a532077e9ef22ead1a8d3f42f87b3c1
Julián David Sánchez Gallego <julian@kommit.co>
2021-12-31 14:04:30 -0500

    Add objects exercises

---
+++ objects/objects/second_exercise.js
@@ -0,0 +1,21 @@
+// Write the function isEmpty(obj) which returns true
+// if the object has no properties, false otherwise.
+
+// Should work like that:
+
+let schedule = {};
+
+alert(isEmpty(schedule)); // true
+
+schedule["8:30"] = "get up";
+
+alert(isEmpty(schedule)); // false
+
+function isEmpty(object) {
+  let empty = true;
+  for (let key in object) {
+    empty = false;
+    break;
+  }
+  return empty;
+}
