eb243f99105a9d5ce67cffc80a0e7fda700bab57
Julián David Sánchez Gallego <julian@kommit.co>
2022-01-07 16:11:19 -0500

    Add the exercise about promise chaining

---
+++ promises/promises_chaining/exercise.js
@@ -0,0 +1,14 @@
+// Are these code fragments equal? In other words,
+// do they behave the same way in any circumstances,
+// for any handler functions?
+
+promise.then(f1).catch(f2);
+
+// Versus:
+
+promise.then(f1, f2);
+
+// In the first code fragment the function f1
+// returns an error that is handled by the
+// following catch but in the second code
+// fragment it returns aerror that is not handled
