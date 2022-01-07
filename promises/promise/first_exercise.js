5d006f8d1c26a8419adff581fc5228bae450066b
Julián David Sánchez Gallego <julian@kommit.co>
2022-01-07 15:12:29 -0500

    Add introduction to promises exercises

---
+++ promises/promise/first_exercise.js
@@ -0,0 +1,12 @@
+// What’s the output of the code below?
+
+let promise = new Promise(function (resolve, reject) {
+  resolve(1);
+
+  setTimeout(() => resolve(2), 1000);
+});
+
+promise.then(alert);
+// It will show 1. The promise returns the first "revolve"
+// or reject that it found. So the las statement within the
+// promise will be ignored
