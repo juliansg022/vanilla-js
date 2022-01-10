1321f49721fb7dfc8eb1a6b8273d87f9e0ebae0d
Julián David Sánchez Gallego <julian@kommit.co>
2022-01-10 18:08:54 -0500

    Add an improvement in the solution syntax

--- second_challenge/solution.js
+++ second_challenge/solution.js
@@ -2,21 +2,16 @@ fetch("https://jsonplaceholder.typicode.com/todos")
   .then((response) => response.json())
   .then((todos) => {
     let todosObject = todos.filter((item) => item.title.includes("delectus"));
-    let todosId = todosObject.reduce(
-      (array, element) => array.concat(element.userId),
-      []
-    );
 
-    let companies = fetch("https://jsonplaceholder.typicode.com/users").then(
-      (response) => response.json()
-    );
+    let todosId = todosObject.reduce((array, element) => array.concat(element.userId), []);
 
-    companies.then((users) => {
-      let companyNames = users.filter((item) => todosId.includes(item.id));
-      window.document.open();
-      companyNames.forEach((item) => {
-        window.document.write(item.company.name + "</br>");
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        let companyNames = users.filter((item) => todosId.includes(item.id));
+
+        window.document.open();
+        companyNames.forEach((item) => window.document.write(item.company.name + "</br>"));
+        window.document.close();
       });
-      window.document.close();
-    });
   });
