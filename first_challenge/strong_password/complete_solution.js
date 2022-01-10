daea29fef6c9a7cc1f77dccdf5612b75a69141d9
Julián David Sánchez Gallego <julian@kommit.co>
2022-01-10 11:45:35 -0500

    Add corrections to the code with JSHint linter

--- first_challenge/strong_password/complete_solution.js
+++ first_challenge/strong_password/complete_solution.js
@@ -1,27 +1,3 @@
-"use strict";
-
-const fs = require("fs");
-
-process.stdin.resume();
-process.stdin.setEncoding("utf-8");
-
-let inputString = "";
-let currentLine = 0;
-
-process.stdin.on("data", function (inputStdin) {
-  inputString += inputStdin;
-});
-
-process.stdin.on("end", function () {
-  inputString = inputString.split("\n");
-
-  main();
-});
-
-function readLine() {
-  return inputString[currentLine++];
-}
-
 /*
  * Complete the 'minimumNumber' function below.
  *
@@ -52,16 +28,5 @@ function minimumNumber(n, password) {
   }
 }
 
-function main() {
-  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-  const n = parseInt(readLine().trim(), 10);
-
-  const password = readLine();
-
-  const answer = minimumNumber(n, password);
-
-  ws.write(answer + "\n");
-
-  ws.end();
-}
+minimumNumber(3, "Ab1");
+minimumNumber(11, "#HackerRank");
