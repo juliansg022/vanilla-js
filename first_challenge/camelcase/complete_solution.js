bf77d288d8a4846d8112fd6b379b27499416d360
Julián David Sánchez Gallego <julian@kommit.co>
2022-01-10 11:40:56 -0500

    Add corrections to the code with JSHint linter

--- first_challenge/camelcase/complete_solution.js
+++ first_challenge/camelcase/complete_solution.js
@@ -1,27 +1,3 @@
-"use strict";
-
-const fs = require("fs");
-
-process.stdin.resume();
-process.stdin.setEncoding("utf-8");
-
-let inputString = "";
-let currentLine = 0;
-
-process.stdin.on("data", function (inputStdin) {
-  inputString += inputStdin;
-});
-
-process.stdin.on("end", function () {
-  inputString = inputString.split("\n");
-
-  main();
-});
-
-function readLine() {
-  return inputString[currentLine++];
-}
-
 /*
  * Complete the 'camelcase' function below.
  *
@@ -46,14 +22,4 @@ function camelcase(s) {
   return words.length;
 }
 
-function main() {
-  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-  const s = readLine();
-
-  const result = camelcase(s);
-
-  ws.write(result + "\n");
-
-  ws.end();
-}
+camelcase("saveChangesInTheEditor");
