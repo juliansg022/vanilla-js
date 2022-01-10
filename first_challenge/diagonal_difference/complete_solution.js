773faaae56866ce38ef7757d160a90ffc869db07
Julián David Sánchez Gallego <julian@kommit.co>
2022-01-10 11:47:53 -0500

    Add corrections to the code with JSHint linter

--- first_challenge/diagonal_difference/complete_solution.js
+++ first_challenge/diagonal_difference/complete_solution.js
@@ -1,27 +1,3 @@
-"use strict";
-
-const fs = require("fs");
-
-process.stdin.resume();
-process.stdin.setEncoding("utf-8");
-
-let inputString = "";
-let currentLine = 0;
-
-process.stdin.on("data", function (inputStdin) {
-  inputString += inputStdin;
-});
-
-process.stdin.on("end", function () {
-  inputString = inputString.split("\n");
-
-  main();
-});
-
-function readLine() {
-  return inputString[currentLine++];
-}
-
 /*
  * Complete the 'diagonalDifference' function below.
  *
@@ -41,23 +17,4 @@ function diagonalDifference(arr) {
   return Math.abs(main - secundary);
 }
 
-function main() {
-  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-  const n = parseInt(readLine().trim(), 10);
-
-  let arr = Array(n);
-
-  for (let i = 0; i < n; i++) {
-    arr[i] = readLine()
-      .replace(/\s+$/g, "")
-      .split(" ")
-      .map((arrTemp) => parseInt(arrTemp, 10));
-  }
-
-  const result = diagonalDifference(arr);
-
-  ws.write(result + "\n");
-
-  ws.end();
-}
+diagonalDifference([[11, 2, 4], [4, 5, 6], [10, 8, -12]]);
