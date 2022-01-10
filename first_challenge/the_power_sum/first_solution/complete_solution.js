4543a42b3834e5a12c67ee4a4f7f1d1e82f427f1
Julián David Sánchez Gallego <julian@kommit.co>
2022-01-10 11:53:05 -0500

    Add corrections to the code with JSHint linter

--- first_challenge/the_power_sum/first_solution/complete_solution.js
+++ first_challenge/the_power_sum/first_solution/complete_solution.js
@@ -1,27 +1,3 @@
-"use strict";
-
-const fs = require("fs");
-
-process.stdin.resume();
-process.stdin.setEncoding("utf-8");
-
-let inputString = "";
-let currentLine = 0;
-
-process.stdin.on("data", function (inputStdin) {
-  inputString += inputStdin;
-});
-
-process.stdin.on("end", function () {
-  inputString = inputString.split("\n");
-
-  main();
-});
-
-function readLine() {
-  return inputString[currentLine++];
-}
-
 /*
  * Complete the 'powerSum' function below.
  *
@@ -34,7 +10,6 @@ function readLine() {
 function powerSum(X, N) {
   // Write your code here
   let limit = parseInt(Math.pow(X, 1 / N));
-  let root = Math.pow(X, 1 / N);
   let ways = 0;
 
   let evaluators = [];
@@ -59,16 +34,7 @@ function powerSum(X, N) {
   return ways;
 }
 
-function main() {
-  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-  const X = parseInt(readLine().trim(), 10);
-
-  const N = parseInt(readLine().trim(), 10);
-
-  const result = powerSum(X, N);
-
-  ws.write(result + "\n");
-
-  ws.end();
-}
+powerSum(10, 2);
+powerSum(100, 2);
+powerSum(100, 3);
+powerSum(800, 2);
--- first_challenge/the_power_sum/second_solution/complete_solution.js
+++ first_challenge/the_power_sum/second_solution/complete_solution.js
@@ -1,27 +1,3 @@
-"use strict";
-
-const fs = require("fs");
-
-process.stdin.resume();
-process.stdin.setEncoding("utf-8");
-
-let inputString = "";
-let currentLine = 0;
-
-process.stdin.on("data", function (inputStdin) {
-  inputString += inputStdin;
-});
-
-process.stdin.on("end", function () {
-  inputString = inputString.split("\n");
-
-  main();
-});
-
-function readLine() {
-  return inputString[currentLine++];
-}
-
 /*
  * Complete the 'powerSum' function below.
  *
@@ -53,16 +29,7 @@ function powerSum(X, N, currentX = 1, accumulated = 0) {
   return ways;
 }
 
-function main() {
-  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-  const X = parseInt(readLine().trim(), 10);
-
-  const N = parseInt(readLine().trim(), 10);
-
-  const result = powerSum(X, N);
-
-  ws.write(result + "\n");
-
-  ws.end();
-}
+powerSum(10, 2);
+powerSum(100, 2);
+powerSum(100, 3);
+powerSum(800, 2);
